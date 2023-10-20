3a54ea2d6083fcbaaab9b9113d913506911ab1b5
Liya Josh <liyajosh07@gmail.com>
2023-10-20 14:37:53 +0530

    Updated Dashboard Story

--- src/components/organisms/dashboard/DashboardStory.tsx
+++ src/components/organisms/dashboard/DashboardStory.tsx
@@ -9,14 +9,27 @@ import {
 import React from "react";
 import { makeStyles, createStyles } from "@mui/styles";
 import { useHistory } from "react-router-dom";
+import AllInclusiveIcon from "@mui/icons-material/AllInclusive";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       backgroundColor: theme.palette.primary.dark,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "baseline",
+      justifyContent: "center",
+      height: "100vh",
     },
     container: {
-      padding: theme.spacing(2),
+      textAlign: "center",
+    },
+    button: {
+      marginRight: theme.spacing(4),
+      marginTop: theme.spacing(2),
+    },
+    text: {
+      marginTop: theme.spacing(4),
     },
   })
 );
@@ -36,12 +49,18 @@ const DashboardStory: React.FC = () => {
     <>
       <Box className={classes.root}>
         <Container maxWidth="md" className={classes.container}>
-          <Typography variant="h6" color="antiquewhite" paragraph>
-            At the heart of our global crowdfunding platform lies a shared
-            vision—a vision where innovation knows no boundaries. Together, we
-            support the dreamers, thinkers, and creators who are driving
-            positive change around the world.
-          </Typography>
+          <AllInclusiveIcon
+            fontSize="large"
+            sx={{ transform: "scale(5)", color: "antiquewhite" }}
+          />
+          <div className={classes.text}>
+            <Typography variant="h5" color="antiquewhite" paragraph>
+              At the heart of our global crowdfunding platform lies a shared
+              vision—a vision where innovation knows no boundaries. Together, we
+              support the dreamers, thinkers, and creators who are driving
+              positive change around the world.
+            </Typography>
+          </div>
           <Stack
             sx={{ pt: 4 }}
             direction="row"
@@ -56,7 +75,6 @@ const DashboardStory: React.FC = () => {
             </Button>
           </Stack>
         </Container>
-        <Container maxWidth="lg"></Container>
       </Box>
     </>
   );
